17f1f39063cc45c7946fd9db9d2ff3e1f1fc5d17
Daniel Abraham <52455128+Danielabraham7@users.noreply.github.com>
2020-04-25 14:24:45 +0530

    Added colors choosing script

---
+++ Scripts/colors.js
@@ -0,0 +1,29 @@
+let text=document.getElementById("field");
+function clrdefault(){
+text.style.color="black";
+}
+function clrViolet(){
+text.style.color="violet";
+}
+function clrIndigo(){
+text.style.color="indigo";
+}
+function clrBlue(){
+text.style.color="blue";
+}
+function clrGreen(){
+text.style.color="green";
+}
+function clrYellow(){
+text.style.color="yellow";
+}
+function clrOrange(){
+text.style.color="orange";
+}
+function clrRed(){
+text.style.color="Red";
+}
+function clrCustom(){
+let CustomColor=prompt("Custom Color (eg:dodgerblue or #000000)");
+text.style.color=CustomColor;
+}
