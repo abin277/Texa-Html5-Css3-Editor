b5a7c8146bcf506e49ef1c7f7723a53bd3ff0504
Daniel Abraham <52455128+Danielabraham7@users.noreply.github.com>
2020-04-28 18:36:55 +0530

    Some updation

--- Scripts/font.js
+++ Scripts/font.js
@@ -1,25 +1,27 @@
+//Font Size (Maximum Size is 24)
 function customFontSize(){
-let customNum=prompt("Font Size in Px(Default 13px Max 24px)");
+let customNum=prompt("Font Size in Px(Default 13px Max 24px)")
 if(isNaN(customNum))
-{
- alert("Sorry This Value Is Not Valid");
-}
-else if(customNum>=25)
-{
-alert("The Maximum value is 24");
-}
+ {
+ alert("You Have Entered A Wrong Value")
+ }
+ elseif (customNum>=25)
+ {
+alert("The Maximum Font Size is 24")
+ }
 else
-{
-document.getElementById("field").style.fontSize=customNum+"px";
-}
-}
+ {
+document.getElementById("field").style.fontSize=customNum+"px"
+ }
+ }
 //FONT STYLE STARTS
+
 function fontMono(){
-document.getElementById("field").style.fontFamily="monospace";
+document.getElementById("field").style.fontFamily="monospace"
 }
 function fontSans(){
-document.getElementById("field").style.fontFamily="Sans-Serif";
+document.getElementById("field").style.fontFamily="Sans-Serif"
 }
 function fontCursive(){
-document.getElementById("field").style.fontFamily="cursive";
+document.getElementById("field").style.fontFamily="cursive"
 }
